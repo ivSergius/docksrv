847e2be79221ce787f3ef2474254227757a7dd21
ivSergius <sisaev@mail.ru>
2016-10-11 01:08:18 +0300

    Update dockerOther.sh

--- dockerOther.sh
+++ dockerOther.sh
@@ -2,3 +2,9 @@ echo Other commands
 
 echo SSH Connect to oracle12c container
 sudo docker exec -i -t $(docker ps -f ancestor=sath89/oracle-12c -q) /bin/bash
+
+echo Start Google cAdvisor on 80 - Docker simple monitor
+docker run --volume=/:/rootfs:ro --volume=/var/run:/var/run:rw  --volume=/sys:/sys:ro --volume=/var/lib/docker/:/var/lib/docker:ro --publish=80:8080 --detach=true --name=cadvisor google/cadvisor:latest
+
+echo Stop Google cAdvisor
+docker stop $(docker ps -a -f ancestor=google/cadvisor -q) && docker rm $(docker ps -a -f ancestor=google/cadvisor -q)
