c10fabced45b43ce9a67e4b854604fac7a2284de
ivSergius <sisaev@mail.ru>
2016-10-10 22:47:18 +0300

    Update dockSrv1.sh

--- dockSrv1.sh
+++ dockSrv1.sh
@@ -1,6 +1,17 @@
+echo Update system
 apt-get update
+
+echo Install MC
 apt-get install -y mc
+
+echo Pull nginx
 docker pull nginx
+
+echo Pull tomcat
 docker pull tomcat
+
+echo Pull ora11
 docker pull sath89/oracle-xe-11g
+
+echo Pull ora12
 docker pull sath89/oracle-12c
