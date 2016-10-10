a622f42d1a510a00b709950f27d4c27e6707c60d
ivSergius <sisaev@mail.ru>
2016-10-10 23:55:10 +0300

    Update dockerRun.sh

--- dockerRun.sh
+++ dockerRun.sh
@@ -1,3 +1,6 @@
 echo Docker RUN script comments
 echo rep: https://hub.docker.com/r/sath89/oracle-12c/
 docker run -d -p 8080:8080 -p 1521:1521 -v /my/oracle12c/data:/u01/app/oracle -e DBCA_TOTAL_MEMORY=1024 sath89/oracle-12c
+
+echo Connect to logs sath89/oracle-12c
+docker logs -f $(docker ps -f ancestor=sath89/oracle-12c -q)
