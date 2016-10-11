4106ddc6eeb39209387faa11ce3799c4a6dbfe0e
ivSergius <sisaev@mail.ru>
2016-10-11 21:54:08 +0300

    Update dockerRun.sh

--- dockerRun.sh
+++ dockerRun.sh
@@ -1,9 +1,13 @@
-echo Docker RUN script comments
-echo rep: https://hub.docker.com/r/sath89/oracle-12c/
-docker run -d -p 8080:8080 -p 1521:1521 -v /my/oracle12c/data:/u01/app/oracle -e DBCA_TOTAL_MEMORY=1024 --name=oracle12c sath89/oracle-12c
+echo #Docker RUN script comments
 
-echo Start Google cAdvisor on 80 - Docker simple monitor
-docker run --volume=/:/rootfs:ro --volume=/var/run:/var/run:rw  --volume=/sys:/sys:ro --volume=/var/lib/docker/:/var/lib/docker:ro --publish=80:8080 --detach=true --name=cadvisor google/cadvisor:latest
+echo #rep: https://hub.docker.com/r/sath89/oracle-12c/
+docker run -d --name oracle12c -p 8080:8080 -p 1521:1521 -v /my/oracle12c/data:/u01/app/oracle -e DBCA_TOTAL_MEMORY=1024 --name=oracle12c sath89/oracle-12c
 
-echo Connect to logs sath89/oracle-12c
+echo #Start Tomcat
+docker run -d --name tomcat -p 8888:8080 -v /my/tomcat/webapps:/usr/local/tomcat/webapps tomcat
+
+echo #Start Google cAdvisor on 80 - Docker simple monitor
+docker run -d --name=cadvisor -p 80:8080  --volume=/:/rootfs:ro --volume=/var/run:/var/run:rw  --volume=/sys:/sys:ro --volume=/var/lib/docker/:/var/lib/docker:ro google/cadvisor:latest
+
+echo #Connect to logs sath89/oracle-12c
 docker logs -f $(docker ps -f ancestor=sath89/oracle-12c -q)
